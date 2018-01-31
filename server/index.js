eb6548a89752a4fa270d2b2caa4edf855674b379
Ofir Zisman <zismanofir@gmail.com>
2018-01-30 18:22:32 -0800

    Clean code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const path = require('path');
-//const db = require('../database/indexMongoDB');
-//const db = require('../database/updateDriver');
-//const db = require('../database/indexMongoDBTry2.js'); 
-const db = require('../database/indexMongoTry3.js');
+//const path = require('path'); 
+const db = require('../database/indexMongo.js');
 
 
 const app = express();
@@ -13,37 +10,22 @@ app.use(bodyParser.json());
 
 //app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-let populateDB = function(start, end, duration) {
-	// insert batches of 50,000 records to database
-	let batch = 50000;
-	db.populateDrivers(start * batch, end * batch, (err, results) => {
-		if (err) {
-			console.log(err);
-		} else {
-			start++;
-			// make it 200 times (recursive call to function)
-			if (start === 200) {
-				console.log(`Duration ${(new Date() - duration) / 1000} s`);
-				return;
-			} 
-			populateDB(start , start + 1, duration);
-		}
-	})
-}
-
 app.get('/', (req, res) => {
   	db.db;
-//	populateDB(0, 1, new Date());
-	let driver = {
-		_id: 5,
-		location: {
-			x: 200,
-			y: 200
-		},
-		availability: 1,
-		activity: 1
-	};
-	db.updateDriver(db.db, driver);
+//	db.populateDB(0, 1, new Date());
+
+	// let driver = {
+	// 	driverId: 5,
+	// 	location: {
+	// 		x: 100,
+	// 		y: 200
+	// 	},
+	// 	availability: 1,
+	// 	activity: 1
+	// };
+	// db.updateDriver(db.db, driver);
+
+	db.updateManyDrivers(0, new Date());
 });
 
 app.listen(3000);
