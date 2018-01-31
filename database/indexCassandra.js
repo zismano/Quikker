c55b8fed9bf3bad912baf0a9346db24457e60761
Ofir Zisman <zismanofir@gmail.com>
2018-01-31 14:10:51 -0800

    Clean code

--- database/indexCassandra.js
+++ database/indexCassandra.js
@@ -4,20 +4,42 @@ var async = require('async');
  //Connect to the cluster
 var client = new cassandra.Client({contactPoints: ['127.0.0.1']});
 
-let insert = post => {
-  let query = "INSERT INTO ks_drivers.drivers (driverId, name, phone, location_x, location_y, availability, activity) VALUES (?,?,?,?,?,?,?)";
-  client.execute(query,[post.driverId, post.name, post.phone, post.location_x, post.location_y, post.availability, post.activity], {prepare: true}, function(err, results){
+let insertDriver = post => {
+  const query = "INSERT INTO ks_drivers.drivers \
+    (driverId, name, phone, location_x, location_y, availability, activity) \
+    VALUES (?,?,?,?,?,?,?)";
+  const params = [
+    post.driverId, 
+    post.name, 
+    post.phone, 
+    post.location_x, 
+    post.location_y, 
+    post.availability, 
+    post.activity
+  ];
+  client.execute(query, params, {prepare: true}, function(err, results){
     if (err) {
       console.log(`Error: ${err}`);
     } else {
       console.log(`Insert: ${results}`);
     }
-  };)
+  });
 }
 
-let updateContact = post => {
-  let query = "UPDATE ks_drivers.drivers SET name = ?, phone = ?, location_x = ?, location_y = ?, availability = ?, activity = ? WHERE driverId = ?";
-  client.execute(query, [post.name, post.phone, post.location_x, post.location_y, post.availability, post.activity, post.driverId], {prepare: true}, function(err, results){
+let updateDriver = post => {
+  const query = "UPDATE ks_drivers.drivers SET \
+    name = ?, phone = ?, location_x = ?, location_y = ?, availability = ?, activity = ? \
+    WHERE driverId = ?";
+  const params = [
+    post.name, 
+    post.phone, 
+    post.location_x, 
+    post.location_y, 
+    post.availability, 
+    post.activity, 
+    post.driverId
+  ];
+  client.execute(query, params, {prepare: true}, function(err, results){
     if (err) {
       console.log(`Error: ${err}`);
     } else {
@@ -26,38 +48,23 @@ let updateContact = post => {
   });
 }
 
-let loadContacts = query => {
-//         var post = req.body;
+let loadDrivers = query => {
   var query = "SELECT * FROM ks_drivers.drivers";
-    // if(typeof(query.email) != "undefined"){
-    //   query += " WHERE email = '"+req.query.email+"'";
-    // }
-    client.execute(query, [], function(err, results) {
-      console.log(results.rows);
-    });
-}
-
-var p = {
-  email: 'pazit@gmail.com',
-  name: 'pazit who?',
-  phone: '444-444',
-  address: 'shenkin',
-  city: 'Tel Aviv'
+  client.execute(query, [], function(err, results) {
+    console.log(results.rows);
+  });
 }
 
 var d = {
-  driverId: 1,
-  name: 'Shifshif',
+  driverId: 2,
+  name: 'Shifa',
   phone: '4321',
   location_x: 33,
   location_y: 999,
   availability: 0,
   activity: 1
 }
-// insert(d);
-//  updateContact(d);
-loadContacts();
-
-// updateContact(p);
-//   loadContacts();
+// insertDriver(d);
+//  updateDriver(d);
+//loadDrivers();
 
