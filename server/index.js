05de7a4208e125f1717bbdaaa33cb039046feb96
Ofir Zisman <zismanofir@gmail.com>
2018-01-30 16:19:54 -0800

    Add invocation to update driver

--- server/index.js
+++ server/index.js
@@ -32,21 +32,18 @@ let populateDB = function(start, end, duration) {
 }
 
 app.get('/', (req, res) => {
- // 	db.db;
-	populateDB(0, 1, new Date());
-	//}
-	// let driver = {
-	// 	_id: 5,
-	// 	location: {
-	// 		x: 200,
-	// 		y: 200
-	// 	},
-	// 	availability: 1,
-	// 	activity: 1
-	// };
-	// db.db;
-	// db.updateDriver(db.db, driver);
-// console.log(db.db);   
+  	db.db;
+//	populateDB(0, 1, new Date());
+	let driver = {
+		_id: 5,
+		location: {
+			x: 200,
+			y: 200
+		},
+		availability: 1,
+		activity: 1
+	};
+	db.updateDriver(db.db, driver);
 });
 
 app.listen(3000);
