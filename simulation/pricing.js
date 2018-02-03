c39919c803bbcb273100ead4e131fb0cd905cf53
Ofir Zisman <zismanofir@gmail.com>
2018-02-03 12:13:51 -0800

    Add simulation of requests from and to Pricing endpoint

---
+++ simulation/pricing.js
@@ -0,0 +1,34 @@
+// Simulating server of Pricing
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+//const path = require('path'); 
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// just for simulation - data inside req.body.params
+app.post('/pricing', (req, res) => {
+  console.log(req.body.params);
+  res.send('Status received');
+})
+
+let sendSurgeRatio = surgeRatio => {
+	axios.post('http://127.0.0.1:3000/pricing', { 
+	  data: {
+	    surgeRatio,
+	  },
+	})
+	.then(results => {
+		console.log(results.data);
+	})
+	.catch(err => {
+		throw err;
+	})
+}
+
+app.listen(4000);
+
+sendSurgeRatio(3.5);
+
