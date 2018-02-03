8e8cbade34e1ff345286d6e5a5ba28f2aadc8d81
Ofir Zisman <zismanofir@gmail.com>
2018-02-03 11:34:31 -0800

    Add cars endpoint for GET request Implement sendStatusNumbersToPricing function with axios POST request to pricing

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 //const path = require('path'); 
@@ -37,13 +38,99 @@ app.get('/', (req, res) => {
 //	 db.countByActivity();
 
 app.get('/cars', (req, res) => {
-//	console.log(req);
-	res.send('Success');
-//	 db.countByActivity();
+// console.log(req);
+  console.log(req.query);
+  let reqActivity = Number(req.query.activity);
+  // check in cache if needs to update, if yes - update cache, send updates to other services, send success to driver
+  db.getDriverStatus({driverId: Number(req.query.driverId)}, (err, result) => {
+
+  	if (err) {
+  	  throw err;
+  	} else if (reqActivity === result.activity && reqActivity) {
+	  res.send('Driver is already online');
+    } else if (reqActivity === result.activity && !reqActivity) {
+	  res.send('Driver is already offline');
+	} else if (result.availablity === 1) {
+	  res.send('Driver has passengers and cannot get offline');
+	} else {
+	  let driver = createDriver(result.driverId, result.name, result.phone, req.query.location.x, req.query.location.y, req.query.activity, req.query.availability);
+
+	  db.updateDriver(driver, (err, result) => {
+	  	if (err) {
+	  	  throw err;
+	  	} else {
+	  	  // SNS (notification) to Matching/Trips
+	  	  // doSomething()
+	  	  if (reqActivity) {
+		    res.send('Driver turned online');
+		    // if response contains user's info (there's a match) send OK
+            console.log('Hello'); 
+		  } else {
+     	    res.send('Driver turned offline');	
+		  } 
+	    }
+	  })
+    }
+  });
+});
+
+let sendStatusNumbersToPricing = () => {
+  db.countDriversByQuery({ activity: 1 }, (err, activeDrivers) => {
+    if (err) {
+      throw err;
+    }
+    db.countDriversByQuery({ availability: 1}, (err, availableDrivers) => {
+      if (err) {
+        throw err;
+      }
+      axios.post('http://127.0.0.1:3000/pricing', {
+        params: {
+          activeDrivers,
+          availableDrivers,
+        },
+      })
+      .then(results => {
+      	console.log(results.data);
+      })
+	  .catch(err => {
+	  	throw err
+	  });            
+    })
+  });
+}
+
+sendStatusNumbersToPricing();
+
+// just for simulation - data inside req.body.params
+// DELETE THIS
+app.post('/pricing', (req, res) => {
+	res.send('Status received');
+})
+
+let createDriver = (driverId, name, phone, locationX, locationY, activity, availability) => {
+  return ({
+  	driverId,
+  	name,
+  	phone,
+  	location: {
+  		x: locationX,
+  		y: locationY,
+  	},
+  	activity,
+  	availability,
+  })
+}
+    // update cache
+    // update other services
+//    req.get('/')
+    // if request was to become offline -> send OK, else, send match
+ //    res.send({userId, userLocation, userDestination});	  	
+ //  } else {
+	// res.send('Request failed due to ...');
+ //  }
+//db.countByActivity();
 //ex.start();
 
-
-});
 
 const d = {
   driverId: 5,
@@ -58,5 +145,6 @@ const d = {
 };
 //db.updateDriver(d);
 
+//setInterval(() => sendStatusNumbersToPricing(), 1000);
 
 app.listen(3000);
