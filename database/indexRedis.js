00b70f1dae596703ee3206773b1580cc160c393b
Ofir Zisman <zismanofir@gmail.com>
2018-02-06 11:14:48 -0800

    Start examples with redis

--- database/indexRedis.js
+++ database/indexRedis.js
@@ -8,3 +8,7 @@ redisClient.on('ready',function() {
 redisClient.on('error',function() {
  console.log("Error in Redis");
 });
+
+redisClient.hmset([1, "availability", 1, "activity", 1, "locationX", 738 ,"locationY", 286], function (err, res) {});
+redisClient.hmset([2, "availability", 1, "activity", 1, "locationX", 2, "locationY", 2], function (err, res) {});
+redisClient.hmset([4, "availability", 0, "activity", 1, "locationX", 159 ,"locationY", 559], function (err, res) {});
