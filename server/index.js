ec1cf09b5f8dc2458e54276289c749264a71c01d
Ofir Zisman <zismanofir@gmail.com>
2018-02-08 11:04:43 -0800

    Add export to app, and default to surgeRatio for tests

--- server/index.js
+++ server/index.js
@@ -6,24 +6,29 @@ const bodyParser = require('body-parser');
 const db = require('../database/indexMongo.js');
 const cache = require('../database/indexRedis.js');
 const push = require('../helpers/awsPush.js');
-//const drivers = require('../client/src/drivers.js');
 const helpers = require('../helpers/helpers.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-//global.surgeRatio = undefined;
-
 app.get('/bla', (req, res) => {
-     db.countDriversByQuery2();
+//     db.countDriversByQuery2();
+	db.insertDriver({
+	  driverId: 10000000,
+	  name: 'Ofir',
+	  phone: '03535353',
+	  location: { x: 2, y: 7},
+	  activity: 1,
+	  availability: 0
+	});
 });
 
 app.get('/cars', (req, res) => {
   console.log(req.query);
   let message = req.query.message; // defined only if driver becomes available 
   let reqActivity = Number(req.query.activity);
-  // check in cache if needs to update, if yes - update cache, send updates to other services, send success to driver
+  // check status of driver
   db.getDriverStatus( {driverId: Number(req.query.driverId) }, (err, result) => {
   	if (err) {
   	  throw err;
@@ -31,13 +36,13 @@ app.get('/cars', (req, res) => {
 	  res.send('Driver is already online');
     } else if (!message && reqActivity === result.activity && !reqActivity) {
 	  res.send('Driver is already offline');
-	} else if (!message && result.activity && result.availability === 0) {
+	} else if (!message && result.activity && Number(result.availability) === 0) {
 	  res.send('Driver has passengers and cannot get offline');
 	} else if (!message && ((new Date() - result.updated_at) / 1000 / 3600) < 2) { // last update 2 hours ago
 	  res.send('Driver may change status every 2 hours and more');
 	} else {
 	  let location = JSON.parse(req.query.location);
-	  let driver = createDriver(result.driverId, result.name, result.phone, location.x, location.y, req.query.activity, req.query.availability);
+	  let driver = helpers.createDriver(result.driverId, result.name, result.phone, location.x, location.y, req.query.activity, req.query.availability);
 	  db.updateDriver(driver, (err, result) => {
 	    if (err) {
 	   	  throw err;
@@ -156,11 +161,12 @@ let sendsAnswerToMatch = (driverId, activity, availability) => {
 
 // fetch price ratio, compare to previous price ratio and send push notifications to some quantity of offline drivers
 app.post('/pricing', (req, res) => {
-  console.log(`Surge ratio is ${req.body.data.surgeRatio}`);
+ // console.log(`Surge ratio is ${req.body.data.surgeRatio}`);
   res.send('Success receiving surge ratio');
+  let ratio = req.body.data.surgeRatio || 1;
   // current surge ratio greater than previous surge ratio
-  if (helpers.surgeRatio && helpers.surgeRatio < req.body.data.surgeRatio) {
-  	let ratio = (req.body.data.surgeRatio - helpers.surgeRatio) * 6;	// *10000 instead of *6
+  if (helpers.surgeRatio && helpers.surgeRatio < ratio) {
+  	ratio = (ratio - helpers.surgeRatio) * 6;	// *10000 instead of *6
     // send push notifications to X offline drivers
   	db.getOfflineDrivers(ratio, (err, results) => {
       if (err) {
@@ -175,24 +181,25 @@ app.post('/pricing', (req, res) => {
     helpers.creationOfOnlineDriversTime /= 1.1;
     helpers.creationOfOfflineDriversTime *= 1.1; 
     // if current surge ratio smaller than previous surge ratio, return to normal 
-  } else if (helpers.surgeRatio && helpers.surgeRatio >= req.body.data.surgeRatio) {
+  } else if (helpers.surgeRatio && helpers.surgeRatio >= ratio) {
     helpers.creationOfOnlineDriversTime = 1;
     helpers.creationOfOfflineDriversTime = 1;     
   }
-  helpers.surgeRatio = req.body.data.surgeRatio;
+  helpers.surgeRatio = ratio;
   drivers.changeDriversInterval(helpers.creationOfOnlineDriversTime, helpers.creationOfOfflineDriversTime); 
 })
 
 app.get('/wait', (req, res) => {
    // check in cache for match of driver
+   //console.log('Req query is:',req.query);
    let start = new Date();
    cache.redisClient.hgetall(req.query.driverId, function(err, object) {
      if (err) {
      	throw err;
      }
      console.log(`Duration check if ${req.query.driverId} is in cache: ${(new Date() - start) / 1000}s`);
-     console.log(object);
-     console.log(`DestX: ${object.destX}, DestY: ${object.destY}`);
+   //  console.log(object);
+   //  console.log(`DestX: ${object.destX}, DestY: ${object.destY}`);
      let data = {};
      data.message = `driver ${req.query.driverId} waits for a match`;
      if (!object) {
@@ -210,7 +217,7 @@ app.get('/wait', (req, res) => {
         data.match = object;
 	    res.send(data);	// send details of user to driver
 	     // update driver's status to not available
-	     let driver = createDriver(req.query.driverId, result.name, result.phone, object.destX, object.destY, 1, 0);
+	     let driver = helpers.createDriver(req.query.driverId, result.name, result.phone, object.destX, object.destY, 1, 0);
 	      db.updateDriver(driver, (err, result) => {
 	   		if (err) {
 	   	      throw err;
@@ -231,19 +238,6 @@ app.get('/wait', (req, res) => {
    });
 });
 
-let createDriver = (driverId, name, phone, locationX, locationY, activity, availability) => {
-  return ({
-  	driverId: Number(driverId),
-  	name,
-  	phone,
-  	location: {
-  		x: Number(locationX),
-  		y: Number(locationY),
-  	},
-  	activity: Number(activity),
-  	availability: Number(activity),
-  })
-}
-
-
 app.listen(3000);
+
+module.exports = app;
