1ce956d545f37fd32ace02ea4b640dd84692d9fe
Ofir Zisman <zismanofir@gmail.com>
2018-02-10 15:10:00 -0800

    Clean code

--- database/mongoUtil.js
+++ database/mongoUtil.js
@@ -1,16 +1,14 @@
 const mongoClient = require('mongodb').MongoClient;
-
 var _db;
 
 module.exports = {
-
   connectToServer: function( callback ) {
     mongoClient.connect( "mongodb://localhost:27017/cars", function( err, db ) {
       _db = db;
       return callback( err );
     } );
   },
-
+  
   getDb: function() {
     return _db;
   }
