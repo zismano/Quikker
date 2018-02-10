a606e05d966deec769891a0a46ad2b7d8f481c37
Ofir Zisman <zismanofir@gmail.com>
2018-02-10 15:08:32 -0800

    Add /wait endpoint

--- performanceTest.js
+++ performanceTest.js
@@ -3,20 +3,19 @@ var siege = require('siege');
 siege()
   .on(3000)
   .for(10000).times
-  .concurrent(25)
   .get('/cars', {siege: true})
   .attack()
 
-// siege()
-//   .on(3000)
-//   .for(10000).times
-//   .get('/cars', {driverId: 5, location: { x: 10, y: 20}, activity: 1, availability: 1})
-//   .attack()
+siege()
+  .on(3000)
+  .for(10000).times
+  .post('/pricing')
+  .attack()
 
-// siege()
-//   .on(3000)
-//   .for(10000).times
-//   .post('/pricing')
-//   .attack()
+siege()
+  .on(3000)
+  .for(10000).times
+  .get('/wait', {siege: true})
+  .attack()
 
 
