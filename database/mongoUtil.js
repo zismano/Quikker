c7d021059cf63df665a4e1806f8e023216ef99ae
Ofir Zisman <zismanofir@gmail.com>
2018-02-16 18:22:08 -0800

    Change to EC2 public ip of mongo instance

--- database/mongoUtil.js
+++ database/mongoUtil.js
@@ -3,7 +3,9 @@ var _db;
 
 module.exports = {
   connectToServer: function( callback ) {
-    mongoClient.connect( "mongodb://localhost:27017/cars", function( err, db ) {
+	mongoClient.connect("mongodb://52.55.217.118:27017/cars", function( err, db ) {	// AWS EC2 instance 
+//    mongoClient.connect( "mongodb://172.17.0.2:27017/cars", function( err, db ) { // docker ip of mongoDB container:mongo port 
+//    mongoClient.connect( "mongodb://localhost:27017/cars", function( err, db ) {
       _db = db;
       return callback( err );
     } );
@@ -13,3 +15,4 @@ module.exports = {
     return _db;
   }
 };
+
