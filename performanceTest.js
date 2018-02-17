dca9d7024b7e47d0903a2c38c9cfe5399d74a530
Ofir Zisman <zismanofir@gmail.com>
2018-02-16 18:19:55 -0800

    Uncomment stress test to endpoints

--- performanceTest.js
+++ performanceTest.js
@@ -1,16 +1,16 @@
 var siege = require('siege');
 
-// siege()
-//   .on(3000)
-//   .for(10000).times
-//   .get('/cars', {siege: true})
-//   .attack()
+siege()
+  .on(3000)
+  .for(10000).times
+  .get('/cars', {siege: true})
+  .attack()
 
-// siege()
-//   .on(3000)
-//   .for(10000).times
-//   .post('/pricing')
-//   .attack()
+siege()
+  .on(3000)
+  .for(10000).times
+  .post('/pricing')
+  .attack()
 
 siege()
   .on(3000)
